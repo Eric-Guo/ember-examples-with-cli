5da260265b2362368c4f20f2f2ac89046108d98b
Eric Guo <eric.guo@sandisk.com>
2015-06-13 07:52:56 +0800

    ch2 example10

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import protoype from './prototype';
 
 let App;
 
@@ -14,5 +15,5 @@ App = Ember.Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
-
+protoype();
 export default App;
---
+++ app/prototype.js
@@ -0,0 +1,16 @@
+export default function(){
+	console.log([1,2,3].get('lastObject')); //3
+
+	var arr = [1,2,3,4,5,6,7].filter(function(item){
+	  if(item < 5){
+	    return true;
+	  }
+	});
+
+	console.log(arr);//[1,2,3,4]
+
+	console.log("ember.js".capitalize());//Ember.js
+	console.log("my var".camelize());//myVar
+	console.log("my var".classify());//MyVar
+  console.log("my var".dasherize());//my-var
+}
