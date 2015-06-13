a48b5d08772cf3305947f93fa56b3fbfa997fad6
Eric Guo <eric.guo@sandisk.com>
2015-06-13 13:30:26 +0800

    ch3 example7

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import truncateHelper from './helpers/truncate';
 
 let App;
 
@@ -15,4 +16,8 @@ App = Ember.Application.extend({
 
 loadInitializers(App, config.modulePrefix);
 
+Ember.Handlebars.registerBoundHelper('truncate', truncateHelper);
+
 export default App;
+
+
