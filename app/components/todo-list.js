071ffa432988c9e2dab5224e6030f9f3125c36c0
Eric Guo <eric.guo@sandisk.com>
2015-06-14 16:36:25 +0800

    todo mvc example

---
+++ app/components/todo-list.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    createTodo: function(newTitle) {
+        this.set('newTitle', '');
+        this.sendAction('createTodo', newTitle);
+    },
+    clearCompleted: function() {
+        var model = this.get('model');
+        var completed = model.filterBy('isCompleted', true);
+        console.log(completed);
+        completed.invoke('deleteRecord');
+        completed.invoke('save');
+    }
+  },
+  remaining: Ember.computed('model.@each.isCompleted', function() {
+    var model = this.get('model');
+    return model.filterBy('isCompleted', false).get('length');
+  }),
+  inflection: Ember.computed('remaining', function() {
+    var remaining = this.get('remaining');
+    return (remaining === 1) ? 'item' : 'items';
+  }),
+  hasCompleted: Ember.computed('completed', function() {
+    return this.get('completed') > 0;
+  }),
+  completed: Ember.computed('model.@each.isCompleted', function() {
+    var model = this.get('model');
+    return model.filterBy('isCompleted', true).get('length');
+  }),
+  allAreDone: Ember.computed('model.@each.isCompleted', {
+    get: function() {
+      var model = this.get('model');
+      return model.get('length') > 0 && model.isEvery('isCompleted', true);
+    },
+    set: function(key, value) {
+      var model = this.get('model');
+      console.log(key + ": " + value);
+      model.setEach('isCompleted', value);
+      model.invoke('save');
+      return value;
+    }
+  })
+});
---
+++ app/routes/todos/complete.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller, model) {
+    this.render('todos.index', {
+      model: model
+    });
+  }
+});
