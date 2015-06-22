569843066db1c49b3f745640365667e8410fdb41
Eric Guo <eric.guo@sandisk.com>
2015-06-22 23:18:29 +0800

    Allow access content for above two site.
    https://www.npmjs.com/package/ember-cli-content-security-policy#example

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      "connect-src": "'self' https://api.github.com/",
+      'img-src':     "'self' https://avatars.githubusercontent.com/"
     }
   };
 
