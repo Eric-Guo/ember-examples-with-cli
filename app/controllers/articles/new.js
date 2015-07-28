0d063685cd3034230adef38ce70d000bc1c12eee
Eric Guo <eric.guo@sandisk.com>
2015-07-28 22:37:57 +0800

    remove login facility

--- app/controllers/articles/new.js
+++ app/controllers/articles/new.js
@@ -1,34 +1,25 @@
 import Ember from 'ember';
-import Firebase from 'firebase';
-import config from '../../config/environment';
 
 export default Ember.Controller.extend({
     session: Ember.inject.service(),
     actions: {
         addArticle: function() {
-            var ref = new Firebase(config.firebase);
-            var authData = ref.getAuth();
-
-            if (authData) {
-                var article = this.store.createRecord('article', {
-                    url: this.get('url'),
-                    title: this.get('title')
-                });
-                this.store.find('user', authData.uid).then(function(user) {
-                    article.set('user', user);
-                    article.save().then(function() {
-                        user.get('articles').addObject(article);
-                        user.save();
-                    });
-                });
-                this.setProperties({
-                    url: '',
-                    title: ''
-                });
-                this.transitionToRoute('articles');
-            } else {
-                console.log('You must log in to post an article');
-            }
+          var article = this.store.createRecord('article', {
+              url: this.get('url'),
+              title: this.get('title')
+          });
+          this.store.find('user', 1).then(function(user) {
+              article.set('user', user);
+              article.save().then(function() {
+                  user.get('articles').addObject(article);
+                  user.save();
+              });
+          });
+          this.setProperties({
+              url: '',
+              title: ''
+          });
+          this.transitionToRoute('articles');
         }
     }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,11 @@
 import Ember from 'ember';
-import Firebase from 'firebase';
-import config from '../config/environment';
 
 export default Ember.Route.extend({
     session: Ember.inject.service(),
     beforeModel: function() {
-        var _this = this;
-        var ref = new Firebase(config.firebase);
-        var authData = ref.getAuth();
-        if (authData) {
-            this.store.find('user', authData.uid).then(function(user) {
-                _this.get('session').set('user', user);
-            });
-        }
+      var _this = this;
+      this.store.find('user', 1).then(function(user) {
+          _this.get('session').set('user', user);
+      });
     }
 });
