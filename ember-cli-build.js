35798c0b1395aadf7698d4dbbd136d32884a8411
Eric Guo <eric.guo@sandisk.com>
2015-08-06 15:14:21 +0800

    ember install ember-cli-materialize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: ['bower_components/materialize/sass']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
