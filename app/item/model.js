dd7ae04ed708f8c4fdb2c59d13f7d4ed87f8204f
Eric Guo <eric.guo@sandisk.com>
2015-08-07 15:35:34 +0800

    Using moment to display start time and end time
    https://github.com/stefanpenner/ember-moment

--- app/item/model.js
+++ app/item/model.js
@@ -1,9 +1,12 @@
 import DS from 'ember-data';
+import momentFromNow from 'ember-moment/computeds/from-now';
 
 export default DS.Model.extend({
   workitem: DS.attr('string'),
   start: DS.attr('date'),
+  startSince: momentFromNow('start'),
   end: DS.attr('date'),
+  endSince: momentFromNow('end'),
   ticketnumber: DS.attr('string'),
   category: DS.attr('string')
 });
