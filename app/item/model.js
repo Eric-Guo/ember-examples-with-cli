62e3c0444eeabf9d9d86fb369c03c3b0183ab6d8
Eric Guo <eric.guo@sandisk.com>
2015-08-07 16:09:12 +0800

    Show duration

--- app/item/model.js
+++ app/item/model.js
@@ -1,5 +1,7 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import momentFromNow from 'ember-moment/computeds/from-now';
+import momentDuration from 'ember-moment/computeds/duration';
 
 export default DS.Model.extend({
   workitem: DS.attr('string'),
@@ -7,6 +9,12 @@ export default DS.Model.extend({
   startSince: momentFromNow('start'),
   end: DS.attr('date'),
   endSince: momentFromNow('end'),
+  duration: Ember.computed('start', 'end', {
+    get() {
+      return this.get('end') - this.get('start');
+    }
+  }),
+  durationMoment: momentDuration('duration', 'milliseconds'),
   ticketnumber: DS.attr('string'),
   category: DS.attr('string')
 });
