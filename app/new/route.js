9d0be7d7fdd3210f8c234d77ca43531fa606b743
Eric Guo <eric.guo@sandisk.com>
2015-08-09 12:00:58 +0800

    New items

--- app/new/route.js
+++ app/new/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    var item;
+    item = this.store.createRecord('item');
+    this.store.findRecord('user', this.get('session').content.secure.user_id).then((u) =>  {
+      item.set('user', u);
+    });
+    return item;
+  },
+  actions: {
+      create: function() {
+        this.controller.get('model').save().then(() => {
+          this.transitionToRoute('items');
+        });
+      }
+  }
+});
