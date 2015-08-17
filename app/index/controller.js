2d5027dfa014a956242be631d4c8f4f60433b95d
Eric Guo <eric.guo@sandisk.com>
2015-08-17 22:49:20 +0800

    Show items count in index page.

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  myItems: Ember.computed('model.@each.user.email', function() {
+    let model = this.get('model');
+    let session = this.get('session');
+    return model.filterBy('user.email', session.content.secure.email);
+  }),
+  myItemsCount: Ember.computed('myItems', function() {
+    return this.get('myItems').get('length');
+  }),
+  myTodayItems: Ember.computed('myItems', function() {
+    let model = this.get('myItems');
+    var today = new Date();
+    today.setTime(today.getTime()-today.getHours()*3600*1000-today.getMinutes()*60*1000);
+    return model.filter((item) => { return (item.get('start') > today); });
+  }),
+  myTodayItemsCount: Ember.computed('myItems', function() {
+    return this.get('myTodayItems').get('length');
+  })
 });
