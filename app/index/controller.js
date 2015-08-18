0a1631d0c725ade97761a81c8ba1d1cb542179f5
Eric Guo <eric.guo@sandisk.com>
2015-08-19 00:23:38 +0800

    Also need to computed the start and end.

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  myItems: Ember.computed('model.@each.user.email', function() {
+  myItems: Ember.computed('model.@each.user','model.@each.start','model.@each.end', function() {
     let model = this.get('model');
     let session = this.get('session');
     return model.filterBy('user.email', session.content.secure.email);
@@ -13,7 +13,7 @@ export default Ember.Controller.extend({
     let model = this.get('myItems');
     var today = new Date();
     today.setTime(today.getTime()-today.getHours()*3600*1000-today.getMinutes()*60*1000);
-    return model.filter((item) => { return (item.get('start') > today); });
+    return model.filter((item) => { return (item.get('start') > today || item.get('end') > today); });
   }),
   myTodayItemsCount: Ember.computed('myItems', function() {
     return this.get('myTodayItems').get('length');
