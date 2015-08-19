4760ae6387e72b1997f8e1663d4a5075f7959ad2
Eric Guo <eric.guo@sandisk.com>
2015-08-19 21:03:19 +0800

    Using helper to display moment instead of add it in model.

--- app/models/item.js
+++ app/models/item.js
@@ -1,21 +1,16 @@
 import Ember from 'ember';
 import DS from 'ember-data';
-import momentFromNow from 'ember-moment/computeds/from-now';
-import momentDuration from 'ember-moment/computeds/duration';
 
 export default DS.Model.extend({
   workitem: DS.attr('string'),
   user: DS.belongsTo('user', {async: true}),
   start: DS.attr('date'),
-  startSince: momentFromNow('start'),
   end: DS.attr('date'),
-  endSince: momentFromNow('end'),
   duration: Ember.computed('start', 'end', {
     get() {
       return this.get('end') - this.get('start');
     }
   }),
-  durationMoment: momentDuration('duration', 'milliseconds'),
   ticketnumber: DS.attr('string'),
   category: DS.attr('string')
 });
